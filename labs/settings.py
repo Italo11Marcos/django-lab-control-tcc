773f2253e1551287d207948925e11cdf47edb91c
italo11marcos <italomarcosboa@gmail.com>
2021-03-03 16:33:46 -0300

    settings

--- labs/settings.py
+++ labs/settings.py
@@ -132,8 +132,8 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'italomarcosboa@gmail.com'
-EMAIL_HOST_PASSWORD = 'wer090122'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 LOGIN_REDIRECT_URL = 'calendario'
 LOGIN_URL = 'login'
